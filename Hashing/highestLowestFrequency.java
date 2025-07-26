6a1b8c040ff927a30a057624185c37fc03caec2e
PPiyush005 <Parashar.piyush2005@gmail.com>
2025-07-26 21:48:08 +0530

    added some file

--- Hashing/highestLowestFrequency.java
+++ Hashing/highestLowestFrequency.java
@@ -9,6 +9,35 @@ public class highestLowestFrequency {
         Scanner sc =  new Scanner(System.in);
 
         int n = sc.nextInt();
-        
+
+        int arr[] = new int[n];
+        for(int i =0 ;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max=arr[0];
+        for(int i = 1; i<n; i++ ){
+            if (max<arr[i]) {
+                max=arr[i];
+            }
+        }
+        max++;
+        int hashh[] = new int[max];
+        for(int i = 0 ; i<n ; i++){
+            hashh[arr[i]]++;
+        }
+
+        int min = hashh[0];
+        int maxi = hashh[0];
+        for (int i = 0; i < hashh.length; i++) {
+    if (hashh[i] > 0 && hashh[i] <= min) {
+        min = hashh[i];
+    }
+    if (hashh[i] > maxi) {
+        maxi = hashh[i];
+    }
+}
+        System.out.println(min);
+        System.out.println(maxi);
     }
 }
