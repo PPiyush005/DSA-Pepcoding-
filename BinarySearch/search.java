9640dbabc4b6ca0427ea0f5293e87fa2e27fd50e
PPiyush005 <Parashar.piyush2005@gmail.com>
2025-08-05 22:38:12 +0530

    Added some file

---
+++ BinarySearch/search.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int low = 0;
+        int high = n;
+        int res = -1;
+      
+        while(low<=high){
+            int mid=(low+high)/2;
+            if (arr[mid]>target) {
+                high = mid-1;
+            }
+            else if (arr[mid]<target) {
+                low = mid+1;
+            }
+            else if (arr[mid]==target) {
+                res = mid;
+            }
+            else{
+            System.out.println(res);
+            }
+            low++;
+            high--;
+        }
+        System.out.println(res);
+    }
+}
